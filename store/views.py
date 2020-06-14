713d7711757750bf3655c3e455835fa13e496c67
Allan Wang <wang.allan@outlook.com>
2020-06-14 13:09:48 +1000

    checkout page can now sync with cart items and orders

--- store/views.py
+++ store/views.py
@@ -27,5 +27,13 @@ def cart(request):
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+     else: ## if not loggin user, items will be empty for now
+               ## TODO: build replica mechinism when user logged in
+               items = []
+               order = {'get_cart_total':0, 'get_cart_items':0}
+     context = {'items':items, 'order':order}
+     return render(request, 'store/checkout.html', context)
