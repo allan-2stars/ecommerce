4a781114fa73031ae619bea8c56bd64bc4330c28
Allan Wang <wang.allan@outlook.com>
2020-06-14 12:38:27 +1000

    cart rendering, make total number sync with database. only done the part for authenticated users.

--- store/models.py
+++ store/models.py
@@ -38,6 +38,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self): ## get the total cart value/price
+        orderitem = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitem])
+        return total
+
+    @property
+    def get_cart_items(self): ## the total quantity of items
+        orderitem = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitem])
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
@@ -46,6 +58,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product.name
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
--- store/views.py
+++ store/views.py
@@ -9,7 +9,21 @@ def store(request):
      return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
+     ## once call cart url/API
+     ## conditions, if user logged in
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          ## get_or_created(defaults=None, **kwargs):
+          ## return: tuple of (object, created)
+          ##   object: the retrieved or creatd object
+          ##   created: boolean whether new object was created
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all() ## get all the order items from the parent order. (childObjects_set)
+     else: ## if not loggin user, items will be empty for now
+          ## TODO: build replica mechinism when user logged in
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0}
+     context = {'items':items, 'order':order}
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
