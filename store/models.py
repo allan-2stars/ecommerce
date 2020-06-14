22b35bd679f23e734d92f2639ee4ac7eac667322
Allan Wang <wang.allan@outlook.com>
2020-06-14 11:39:35 +1000

    add null=True on order BooleanFiled

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True) ## set customer to Null if customer deleted
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False, null=True)
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
